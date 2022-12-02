ca5e47bfd0101b629748873113a69f05cab8e4a3
alimbekovbayel <117744738+alimbekovbayel@users.noreply.github.com>
2022-12-02 18:18:47 +0600

    parcing
    parcing

--- main.py
+++ main.py
@@ -1,54 +1,42 @@
-import csv
+# import requests
+# from bs4 import BeautifulSoup
+# import json
+# URl = "https://news.ycombinator.com/  "
+HEADERS = {
+     'user-agent': 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+     }
 import requests
+from pprint import pprint
 from bs4 import BeautifulSoup
+import json
 
-URL = 'https://www.kivano.kg/noutbuki'
-CSV = 'kivano_noutbuki.csv'
-HOST = 'https://www.kivano.kg/'
-HEADERS = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
 
-}
 
-def get_html(url, params=''):
-    r = requests.get(url, headers=HEADERS, params=params, verify=False)
-    return r
-
+URL = 'https://news.ycombinator.com/'
+def get_html(url):
+    r = requests.get(url)
+    return r.content
 
 def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
-    items = soup.findAll('div', class_="item product_listbox oh")
-    laptops = []
+    items = soup.findAll('tr', class_='athing')
+    new_list = []
     for item in items:
-        laptops.append({
-            'Название': item.find('div', class_='listbox_title oh').get_text(),
-            'Описание': str(item.find('div', class_='product_text pull-left').get_text()).split('\n')[-1],
-            'Фото' :  'https://www.kivano.kg' + item.find('img').get('src'),
-            'Цена' : item.find('div', class_='listbox_price text-center').get_text(),
-            'Подробнее' : 'https://www.kivano.kg' + item.find('div', class_='listbox_title oh').find('a').get('href'),
-            'Наличие' : item.find('div', class_='listbox_motive text-center').get_text()
+        new_list.append({
+            'Title' : item.find('span', class_='titleline').get_text(),
+            'Title Link' : item.find('span', class_='titleline').find('a').get('href'),
+            'Point': (soup.find ('span',class_='score').get_text()),
+            'HOUR':(soup.find('span',class_='age').get_text()),
+            'COMENTS':str(soup.find('span',class_='subline').get_text().split()[-2]),
+            'COMENTS LINK':'https://news.ycombinator.com/' + soup.find('span',class_='age').find('a').get('href'),
         })
-    return laptops
+    return new_list
 
-def save(items, path):
-    with open(path, 'w') as file:
-        writer = csv.writer(file, delimiter=';')
-        writer.writerow(['Название', 'Описание', 'Фото', 'Цена', 'Подробнее','Наличие'])
-        for item in items:
-            writer.writerow([item['Название'], item['Описание'], item['Фото'], item['Цена'], item['Подробнее'], item['Наличие']])
+def save(data):
+    with open('data.json', 'w') as file:
+        json.dump(data, file, indent=4)
+        pprint(data)
+save(get_content(get_html(URL)))
 
-def parser():
-    PAGENATION = int(input('Введите номер страницы: ').strip())
-    html = get_html(URL)
-    if html.status_code == 200:
-        new_list = []
-        for page in range(1, PAGENATION+1):
-            print(f'Страница {page} готова')
-            html = get_html(URL, params={'page' : page})
-            new_list.extend(get_content(html.text))
-        save(new_list, CSV)
-        print('Завершено')
-    else:
-        print('Нет соединения')
 
-if __name__ == '__main__':
-    parser()
+
